f06fe9721655f1966a3bbfef44d03d2a9b169146
AntoninMartin <antonin.martin.etu@gmail.com>
2016-12-06 19:31:35 +0100

    Android notification
    Notification update

--- NotiFact/NotiFact.Droid/MainActivity.cs
+++ NotiFact/NotiFact.Droid/MainActivity.cs
@@ -6,12 +6,17 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using System.Threading.Tasks;
+using Firebase.Iid;
+using Firebase.Messaging;
 
 namespace NotiFact.Droid
 {
     [Activity(Label = "NotiFact", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
+		private const string GcmSenderId = "672494234051";
+
         protected override void OnCreate(Bundle bundle)
         {
             TabLayoutResource = Resource.Layout.Tabbar;
@@ -22,6 +27,26 @@ namespace NotiFact.Droid
             global::Xamarin.Forms.Forms.Init(this, bundle);
             LoadApplication(new App());
         }
+
+		public void RegisterToNotifications()
+		{
+			Task.Run(() =>
+			{
+				try
+				{
+					var instanceID = FirebaseInstanceId.Instance;
+					instanceID.DeleteInstanceId();
+					var iid1 = instanceID.Token;
+					var token = instanceID.GetToken(GcmSenderId, FirebaseMessaging.InstanceIdScope);
+					System.Diagnostics.Debug.WriteLine($"Iid1 : {iid1}");
+					System.Diagnostics.Debug.WriteLine($"Iid2 : {token}");
+				}
+				catch (Exception ex)
+				{
+					System.Diagnostics.Debug.WriteLine(ex);
+				}
+			});
+		}
     }
 }
 
---
+++ NotiFact/NotiFact.Droid/NotiFactFirebaseMessagingService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Media;
+using Android.Support.V4.App;
+using Firebase.Messaging;
+
+namespace NotiFact.Droid
+{
+	[Service]
+	[IntentFilter(new[] { "com.google.firebase.MESSAGING_EVENT" })]
+	public class NotiFactFirebaseMessagingService : FirebaseMessagingService
+	{
+		public override void OnMessageReceived(RemoteMessage message)
+		{
+			var notification = message.GetNotification();
+			if (notification != null)
+			{
+				System.Diagnostics.Debug.WriteLine($"FirebaseMessagingService - From {message.From}");
+				this.SendNotification(notification);
+			}
+		}
+
+		void SendNotification(RemoteMessage.Notification notification)
+		{
+			var intent = new Intent(this, typeof(MainActivity));
+			intent.AddFlags(ActivityFlags.ClearTop);
+
+			var pendingIntent = PendingIntent.GetActivity(this, 0, intent, PendingIntentFlags.OneShot);
+
+			var defaultSoundUri = RingtoneManager.GetDefaultUri(RingtoneType.Notification);
+			var notificationBuilder = new NotificationCompat.Builder(this)
+															.SetContentTitle(notification.Title)
+															.SetSmallIcon(Resource.Drawable.icon)
+															.SetContentText(notification.Body)
+															.SetAutoCancel(true)
+															.SetSound(defaultSoundUri)
+															.SetContentIntent(pendingIntent);
+
+			var notificationManager = (NotificationManager)GetSystemService(Context.NotificationService);
+			notificationManager.Notify(0, notificationBuilder.Build());
+		}
+	}
+}
